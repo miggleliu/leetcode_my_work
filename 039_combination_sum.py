3e599d8ccd372019c90d7b4980b07cff541b16f2
Boxi Liu <56619652+miggleliu@users.noreply.github.com>
2021-07-13 10:30:47 -0400

    Update 039_combination_sum.py

--- 039_combination_sum.py
+++ 039_combination_sum.py
@@ -1,3 +1,6 @@
+'''
+# recursion
+
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
@@ -30,6 +33,38 @@ class Solution:
             ans += prev
             
         return ans
+'''
 
 
+# DFS, backtracking
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        # sort the candidates in decending order so that we can check the biggest element first
+        # fill the bottle with stones firstly and sands lastly
+        candidates.sort()
+        candidates.reverse()
+        ans = []
+        
+        def backtracking(idx, target, combination):
+            if target == 0:
+                ans.append(combination.copy())
+                return
+            
+            if target < 0:
+                return
+            
+            # if we do not pick the large element, we cannot pick it afterwards. This is what idx is doing
+            for i in range(idx, len(candidates)):
+                backtracking(i, target - candidates[i], combination + [candidates[i]])
+                
+                
+        backtracking(0, target, [])
+        return ans
+            
+            
+                
+        
+        
 
